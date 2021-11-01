eb8dd9c3c771c76e2e567e10fed1405f536a35ff
IlyaTrubchik <92310654+IlyaTrubchik@users.noreply.github.com>
2021-11-01 22:14:32 +0300

    Update MainActivity.java

--- src/main/java/converter/tms/com/converter/MainActivity.java
+++ src/main/java/converter/tms/com/converter/MainActivity.java
@@ -1,10 +1,7 @@
 package converter.tms.com.converter;
 
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
-import android.text.InputType;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,100 +12,101 @@ import android.widget.Spinner;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.widget.TextView;
-
-import java.math.BigInteger;
 import java.util.Locale;
+import android.support.annotation.RequiresApi;
+import android.text.InputType;
+import android.os.Build;
+import java.math.BigInteger;
 
 public class MainActivity extends Activity {
 
-    private String[] syst_ish = {"2-чной","8-чной","10-чной","16-чной"};
-    private String[] syst_kon = {"2-чную","8-чную","10-чную","16-чную"};
+        private String[] ish = {"2-чной","8-чной","10-чной","16-чной"};
+        private String[] kon = {"2-чную","8-чную","10-чную","16-чную"};
 
-
+        
     ClipboardManager clipboardManager ;
     ClipData clipData;
     TextView textCopy;
-    String Number;
-    int a,b;
+    String number;
+    ImageButton c_btn;
+    int a;
+    int b;    
     Boolean check;
     EditText editText;
-
-
-
-
+        
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            
+            editText = findViewById(R.id.editTextTextNumber);
+            textCopy = findViewById(R.id.result);
+            
+            clipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
 
-        editText = findViewById(R.id.editTextTextNumber);
-        textCopy = findViewById(R.id.result);
-
-        clipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
-        ImageButton c_btn= findViewById(R.id.c_btn);
-
-        ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, syst_ish);
-        Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner spinner= (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(Adapter);
-
-        AdapterView.OnItemSelectedListener itemSelectedListener1 = new AdapterView.OnItemSelectedListener() {
+        ArrayAdapter<String> adapt = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, ish);
+     adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+     Spinner spinner= (Spinner) findViewById(R.id.spinner);
+     spinner.setAdapter(adapt);
+            
+                    AdapterView.OnItemSelectedListener itemSelectedListener1 = new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position1, long id) {
 
                 if (position1 == 0) {
                     a= 2;
                 }
-                if (position1 == 1) {
+                else if (position1 == 1) {
                     a= 8;
                 }
-                if (position1 == 2) {
+                else if (position1 == 2) {
                     a= 10;
                 }
-                if (position1 == 3) {
+                else if (position1 == 3) {
                     a= 16;
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+                    throw new UnsupportedOperationException();
             }
         };
         spinner.setOnItemSelectedListener(itemSelectedListener1);
 
-        ArrayAdapter<String> Adapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, syst_kon);
-        Adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapt1 = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, kon);
+        adapt1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner1= (Spinner) findViewById(R.id.spinner1);
-        spinner1.setAdapter(Adapter1);
-
-        AdapterView.OnItemSelectedListener itemSelectedListener2 = new AdapterView.OnItemSelectedListener() {
+        spinner1.setAdapter(adapt1);
+            
+            AdapterView.OnItemSelectedListener itemSelectedListener2 = new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position2, long id) {
 
                 if (position2 == 0) {
                     b= 2;
                 }
-                if (position2 == 1) {
+                else if (position2 == 1) {
                     b= 8;
                 }
-                if (position2 == 2) {
+                else if (position2 == 2) {
                     b= 10;
                 }
-                if (position2 == 3) {
+                else if (position2 == 3) {
                     b= 16;
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+                    throw new UnsupportedOperationException();
             }
         };
 
 
         spinner1.setOnItemSelectedListener(itemSelectedListener2);
-
-
-
+            
+             
         editText.setOnKeyListener(new View.OnKeyListener()
                                   {
                                       public boolean onKey(View v, int keyCode, KeyEvent event)
@@ -116,28 +114,24 @@ public class MainActivity extends Activity {
                                           if(event.getAction() == KeyEvent.ACTION_DOWN &&
                                                   (keyCode == KeyEvent.KEYCODE_ENTER))
                                           {
-                                              Number = editText.getText().toString();
+                                              // сохраняем текст, введённый до нажатия Enter в переменную
+                                              number = editText.getText().toString();
                                               return true;
                                           }
                                           return false;
                                       }
                                   }
         );
-
-
-
+            
     }
-
-    public void CopyClick(View view) {
+        public void CopyClick(View view) {
         clipData = ClipData.newPlainText("text",textCopy.getText());
         clipboardManager.setPrimaryClip(clipData);
 
     }
 
 
-
     public void Convertize(View view) {
-
         Number = editText.getText().toString();
         if (a==2){
             if (Number.matches("[0-1-]+"))
@@ -149,7 +143,7 @@ public class MainActivity extends Activity {
                 textCopy.setText("Error!");
             }
         }
-
+        
         if (a==8){
             if (Number.matches("[0-7-]+"))
             {
