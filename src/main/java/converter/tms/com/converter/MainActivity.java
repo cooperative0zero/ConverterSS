9bbca8c99e1c463d14096693a1229955cb2ae743
cooperative0zero <man747704@gmail.com>
2021-11-16 17:16:08 +0300

    Update MainActivity.java
    This commit contains changes to the visual part of the main page of the application.

--- src/main/java/converter/tms/com/converter/MainActivity.java
+++ src/main/java/converter/tms/com/converter/MainActivity.java
@@ -2,33 +2,34 @@ package converter.tms.com.converter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.text.method.ScrollingMovementMethod;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.widget.TextView;
-import java.util.Locale;
-import android.support.annotation.RequiresApi;
-import android.text.InputType;
-import android.os.Build;
 import java.math.BigInteger;
+import android.widget.CompoundButton;
 
 public class MainActivity extends Activity {
 
         private String[] ish = {"2-чной","8-чной","10-чной","16-чной"};
         private String[] kon = {"2-чную","8-чную","10-чную","16-чную"};
 
-        
+
     ClipboardManager clipboardManager ;
     ClipData clipData;
     TextView textCopy;
-    String Number;
+    String number;
     ImageButton c_btn;
     int a;
     int b;    
@@ -39,17 +40,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            
-            editText = findViewById(R.id.editTextTextNumber);
-            textCopy = findViewById(R.id.result);
-            
-            clipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+        editText = findViewById(R.id.editTextTextNumber);
+        textCopy = findViewById(R.id.result);
+        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+
+
 
         ArrayAdapter<String> adapt = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, ish);
      adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
      Spinner spinner= (Spinner) findViewById(R.id.spinner);
      spinner.setAdapter(adapt);
-            
+
                     AdapterView.OnItemSelectedListener itemSelectedListener1 = new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position1, long id) {
@@ -116,7 +118,7 @@ public class MainActivity extends Activity {
                                                   (keyCode == KeyEvent.KEYCODE_ENTER))
                                           {
                                               // сохраняем текст, введённый до нажатия Enter в переменную
-                                              Number = editText.getText().toString();
+                                              number = editText.getText().toString();
                                               return true;
                                           }
                                           return false;
@@ -126,65 +128,71 @@ public class MainActivity extends Activity {
             
     }
         public void CopyClick(View view) {
-        clipData = ClipData.newPlainText("text",textCopy.getText());
-        clipboardManager.setPrimaryClip(clipData);
+            clipData = ClipData.newPlainText("text", textCopy.getText());
+            clipboardManager.setPrimaryClip(clipData);
 
-    }
-
+        }
 
     public void Convertize(View view) {
-        Number = editText.getText().toString();
+        number = editText.getText().toString();
         if (a==2){
-            if (Number.matches("[0-1-]+"))
+            if (number.matches("[0-1-]+"))
             {
-                String c = new String(new BigInteger(Number, a).toString(b));
+                textCopy.setTextColor(getResources().getColor(R.color.green));
+                String c = new String(new BigInteger(number, a).toString(b));
                 textCopy.setText(String.valueOf(c.toUpperCase()));
             }
             else{
-                textCopy.setText("Error!");
+                textCopy.setTextColor(getResources().getColor(R.color.red));
+                textCopy.setText(getResources().getString(R.string.errror));
             }
         }
-        
+
         if (a==8){
-            if (Number.matches("[0-7-]+"))
+            if (number.matches("[0-7-]+"))
             {
-                String c = new String(new BigInteger(Number, a).toString(b));
+                textCopy.setTextColor(getResources().getColor(R.color.green));
+                String c = new String(new BigInteger(number, a).toString(b));
                 textCopy.setText(String.valueOf(c.toUpperCase()));
             }
             else{
-                textCopy.setText("Error!");
+                textCopy.setTextColor(getResources().getColor(R.color.red));
+                textCopy.setText(getResources().getString(R.string.errror));
             }
         }
         if (a==10){
-            if (Number.matches("[0-9-]+"))
+            if (number.matches("[0-9-]+"))
             {
-                String c = new String(new BigInteger(Number, a).toString(b));
+                textCopy.setTextColor(getResources().getColor(R.color.green));
+                String c = new String(new BigInteger(number, a).toString(b));
                 textCopy.setText(String.valueOf(c.toUpperCase()));
             }
             else{
-                textCopy.setText("Error!");
+                textCopy.setTextColor(getResources().getColor(R.color.red));
+                textCopy.setText(getResources().getString(R.string.errror));
             }
         }
         if (a==16){
 
 
                 check=false;
-                if (Number.matches("[0-9a-fA-F-]+")) {
-                    String c = new String(new BigInteger(Number, a).toString(b));
+                if (number.matches("[0-9a-fA-F-]+")) {
+                    textCopy.setTextColor(getResources().getColor(R.color.green));
+                    String c = new String(new BigInteger(number, a).toString(b));
                     textCopy.setText(String.valueOf(c.toUpperCase()));
                     check = true;
                 }
 
                 if (check == false) {
-                    textCopy.setText("Error!");
+                    textCopy.setTextColor(getResources().getColor(R.color.red));
+                    textCopy.setText(getResources().getString(R.string.errror));
                 }
             }
 
-
     }
-
     public void OpenHistory(View view) {
         Intent intent = new Intent(this,HistoryActivity.class);
         startActivity(intent);
     }
+
 }
